8a48e8953ef0acd10da2faae49ad7abf730e1fe2
cmolina12 <c.molinap@uniandes.edu.co>
2025-06-14 16:38:22 -0500

    spring application

---
+++ src/main/java/com/cmolina12/flowplan_backend/FlowplanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cmolina12.flowplan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowplanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowplanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmolina12/flowplan_backend/FlowplanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmolina12.flowplan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowplanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
