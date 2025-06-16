dd027edf3e058a2cf3d6f5395ac40a0dd03630eb
cmolina12 <c.molinap@uniandes.edu.co>
2025-06-16 13:33:45 -0500

    parser Time, parser Days

--- src/main/java/com/cmolina12/flowplan_backend/service/CourseService.java
+++ src/main/java/com/cmolina12/flowplan_backend/service/CourseService.java
@@ -1,8 +1,14 @@
 package com.cmolina12.flowplan_backend.service;
 import com.cmolina12.flowplan_backend.models.ApiCourse;
+import com.cmolina12.flowplan_backend.models.Schedule;
+
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.ArrayList;
+import java.time.DayOfWeek;
 
 @Service
 public class CourseService {
@@ -37,5 +43,33 @@ public class CourseService {
         // The RestTemplate is used to make a GET request to the constructed URL, and the response is expected to be an array of ApiCourse objects.
         return restTemplate.getForObject(url, ApiCourse[].class);
     }
+
+    /**
+     * Parses a time string in the format "hhmm" and returns a LocalTime object.
+     * 
+     * @param hhmm the time string in "hhmm" format.
+     * @return a LocalTime object representing the parsed time.
+     */
+    private LocalTime parseTime(String hhmm){
+        int hour = Integer.parseInt(hhmm.substring(0, 2)); // Extracts the hour part from the hhmm string.
+        int minute = Integer.parseInt(hhmm.substring(2, 4)); // Extracts the minute part from the hhmm string.
+        return LocalTime.of(hour, minute); // Returns a LocalTime object representing the time.
+    }
+
+    private List<DayOfWeek> parseDays(Schedule s){
+        List<DayOfWeek> days = new ArrayList<>(); // Initializes an empty list to hold the days of the week.
+
+        if ("L".equalsIgnoreCase(s.getL())) days.add(DayOfWeek.MONDAY);
+        if ("M".equalsIgnoreCase(s.getM())) days.add(DayOfWeek.TUESDAY);
+        if ("I".equalsIgnoreCase(s.getI())) days.add(DayOfWeek.WEDNESDAY);
+        if ("J".equalsIgnoreCase(s.getJ())) days.add(DayOfWeek.THURSDAY);
+        if ("V".equalsIgnoreCase(s.getV())) days.add(DayOfWeek.FRIDAY);
+        if ("S".equalsIgnoreCase(s.getS())) days.add(DayOfWeek.SATURDAY);
+        
+        return days; // Returns the list of days of the week.
+
+
+    }
+
 
 }
