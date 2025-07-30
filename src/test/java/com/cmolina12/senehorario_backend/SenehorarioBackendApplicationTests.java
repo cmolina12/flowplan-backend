d1e6bd7a3a0bf7d699d413361394ccdcbf1115a8
cmolina12 <c.molinap@uniandes.edu.co>
2025-07-30 16:13:08 -0500

    added Docker file for deployment

--- src/test/java/com/cmolina12/senehorario_backend/SenehorarioBackendApplicationTests.java
+++ src/test/java/com/cmolina12/senehorario_backend/SenehorarioBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cmolina12.flowplan_backend;
+package com.cmolina12.senehorario_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
